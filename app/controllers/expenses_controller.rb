3abdcdaeab77487cac6a173ded603697aec72f50
Tiago Marin <tiago.lelinski@gmail.com>
2022-12-10 00:02:06 -0300

    add styles to views to match design

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,31 +1,32 @@
 class ExpensesController < ApplicationController
-  before_action :set_expense, only: %i[ show edit update destroy ]
+  before_action :set_user
+  before_action :set_category
+  before_action :set_expense, only: %i[show edit update destroy]
 
   # GET /expenses or /expenses.json
   def index
-    @expenses = Expense.all
+    @expenses = @category.expenses
   end
 
   # GET /expenses/1 or /expenses/1.json
-  def show
-  end
+  def show; end
 
   # GET /expenses/new
   def new
-    @expense = Expense.new
+    @expense = @user.expenses.build
+    @expense.category_ids = @category.id
   end
 
   # GET /expenses/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /expenses or /expenses.json
   def create
-    @expense = Expense.new(expense_params)
+    @expense = @user.expenses.build(expense_params)
 
     respond_to do |format|
       if @expense.save
-        format.html { redirect_to expense_url(@expense), notice: "Expense was successfully created." }
+        format.html { redirect_to category_path(@expense.category_ids), notice: 'Expense was successfully created.' }
         format.json { render :show, status: :created, location: @expense }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +39,9 @@ class ExpensesController < ApplicationController
   def update
     respond_to do |format|
       if @expense.update(expense_params)
-        format.html { redirect_to expense_url(@expense), notice: "Expense was successfully updated." }
+        format.html do
+          redirect_to category_url(@category), notice: 'Expense was successfully updated.'
+        end
         format.json { render :show, status: :ok, location: @expense }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,19 +55,28 @@ class ExpensesController < ApplicationController
     @expense.destroy
 
     respond_to do |format|
-      format.html { redirect_to expenses_url, notice: "Expense was successfully destroyed." }
+      format.html { redirect_to category_path(@category), notice: 'Expense was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_expense
-      @expense = Expense.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def expense_params
-      params.require(:expense).permit(:name, :amount)
-    end
+  def set_user
+    @user = current_user
+  end
+
+  def set_category
+    @category = Category.find(params[:category_id])
+  end
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_expense
+    @expense = Expense.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def expense_params
+    params.require(:expense).permit(:name, :amount, category_ids: [])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :categories, inverse_of: 'user', foreign_key: 'user_id', dependent: :destroy
+  has_many :expenses, inverse_of: 'user', foreign_key: 'user_id', dependent: :destroy
+
+  validates :name, presence: true, uniqueness: true,
+                   length: { maximum: 27, too_long: '%<count>s characters is the maximum allowed' }
 end
